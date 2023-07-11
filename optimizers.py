7f96c41d044948e61371607aadcc95369a4fd464
Erfan Moeini <moeini.erfan@gmail.com>
2023-07-12 00:07:33 +0330

    use total sum

--- optimizers.py
+++ optimizers.py
@@ -27,42 +27,22 @@ class TiAda(Optimizer):
         self.alpha = alpha
         self.opponent_optim = opponent_optim
 
-        self.total_sum = self.param_groups[0]["params"][0].new_zeros(1)
-        with torch.no_grad():
-            for p in self.params:
-                state = self.state[p]
-                init_value = 0
-                state["sum"] = torch.full_like(
-                    p, init_value, memory_format=torch.preserve_format
-                )
+        self.v = torch.Tensor([0])
 
-                self.total_sum.add_(state["sum"].sum())
-
+        
     @torch.no_grad()
     def step(self):
         loss = None
-
         for p in self.params:
             if p.grad is not None:
                 grad2 = torch.mul(p.grad, p.grad)
-                self.state[p]["sum"].add_(grad2)
-                self.total_sum.add_(grad2.sum())
+                self.v.add_(grad2.sum())
 
+        ratio = self.v.pow(self.alpha)
         if self.opponent_optim is not None:
-            ratio = self.total_sum.pow(self.alpha)
-            ratio.div_(
-                    torch.max(
-                        ratio,
-                        self.opponent_optim.total_sum.pow(self.alpha)
-                        )
-                    )
-        else:
-            ratio = 1
+            ratio = torch.max(ratio, self.opponent_optim.v.pow(self.alpha))
 
         for p in self.params:
             if p.grad is not None:
-                state_sum = self.state[p]["sum"]
-                diff =p.grad.mul_(ratio).div_(state_sum.pow(self.alpha).add_(1e-10))
-                p.sub_(diff.mul_(self.lr))   
-
+                p.sub_(p.grad.mul_(self.lr).div_(ratio.add_(1e-10)))
         return loss
